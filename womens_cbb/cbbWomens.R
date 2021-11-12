e48899d5a48bb2b3a074fd0bbd48e149fab8d735
Lucas Nelson <lucas.nelson.uiuc@gmail.com>
2021-11-11 21:39:44 -0600

    finalize supply chain

--- womens_cbb/cbbWomens.R
+++ womens_cbb/cbbWomens.R
@@ -4,9 +4,18 @@ library(glue)
 library(xlsx)
 
 
+user.input = function(prompt) {
+  if (interactive()) {
+    return(readline(prompt))
+  } else {
+    cat(prompt)
+    return(readLines("stdin", n=1))
+  }
+}
+
 validate_date = function(query.date) {
   
-  while (query.date < 2002 | query.date > as.numeric(format(Sys.Date(), '%Y'))) {
+  while (query.date < 2002 | query.date > as.numeric(format(Sys.Date(), '%Y')) + 1) {
     print(glue('Invalid date entered! Year must exist between 2002 - {format(Sys.Date(), \'%Y\')}'))
     query.date = as.numeric(readline('Enter season end year (e.g. 2022 for 2021-22 season): '))
   }
@@ -43,34 +52,32 @@ validate_query = function(home.team, away.team) {
   
 }
 
-main = function() {
-  valid.start = validate_date(as.numeric(readline('Enter season end year (e.g. 2022 for 2021-22 season): ')))
-  valid.teams = validate_teams() %>% select(-X)
-  valid.query = validate_query(str_to_title(readline('Enter home team: ')),
-                               str_to_title(readline('Enter away team: ')))
+extract_team_data = function(team.name, valid.start, relative.season) {
+  # create key for home.team
+  team.info = valid.teams %>% filter(team == team.name)
   
-  # assign home team names
-  home.team = valid.query[[1]][1]
-  # away.team = valid.teams[[2]][1]
-  
-  # create information matrix for each team
-  home.info = valid.teams %>% filter(team == home.team) %>% select(team_id, team)
+  # update date query if previous season requested
+  if (relative.season == 'previous') {valid.start = valid.start - 1}
+  print(glue('  == QUERY == {team.name} (team) {valid.start - 1}-{valid.start} (season)'))
   
   # gather previous season game statistics using game IDs
-  home.previous_game_ids = load_wbb_team_box(valid.start) %>% filter(team_id == home.info$team_id) %>%
+  team.previous_game_ids = load_wbb_team_box(valid.start) %>% 
+    filter(team_id == team.info$team_id) %>%
     select(game_id)
   
-  print(glue('  > Extracting {valid.start - 1} data for {home.team} ...'))
-  dfs = apply(home.previous_game_ids, 1, espn_wbb_game_all)
+  if (relative.season == 'lastfive') {team.previous_game_ids %>% slice_tail(n=5)}
   
-  print(glue('  > Merging {home.team} {valid.start - 1} data.frames ...'))
+  print(glue('    ... Extracting {valid.start - 1}-{valid.start} data for {team.name}'))
+  dfs = apply(team.previous_game_ids, 1, espn_wbb_game_all)
+  
+  print(glue('    ... Merging {team.name} {valid.start - 1}-{valid.start} data.frames'))
   player_df_list = list()
   for (i in 1:length(dfs)) {
     player_df_list[[i]] = dfs[[i]]$Player
   }
   
-  home.big_data = dplyr::bind_rows(player_df_list) %>%
-    filter(team_id == home.info$team_id) %>%
+  team.statistics_batch_01 = dplyr::bind_rows(player_df_list) %>%
+    filter(team_id == team.info$team_id) %>%
     select(athlete_display_name, min, pts,
            reb, ast) %>%
     group_by(athlete_display_name) %>%
@@ -82,8 +89,8 @@ main = function() {
       AST = round(mean(as.numeric(ast), na.rm=TRUE), 1)
     )
   
-  home.shooting_data = dplyr::bind_rows(player_df_list) %>%
-    filter(team_id == home.info$team_id) %>%
+  team.statistics_batch_02 = dplyr::bind_rows(player_df_list) %>%
+    filter(team_id == team.info$team_id) %>%
     select(athlete_display_name, fg, ft) %>%
     extract(fg, c('fga', 'fgm'), '(.)-(.)') %>%
     extract(ft, c('fta', 'ftm'), '(.)-(.)') %>%
@@ -98,61 +105,65 @@ main = function() {
            ftp = round(FTA / (FTA + FTM) * 100, 1)) %>%
     select(athlete_display_name, fgp, ftp)
   
+  # merge data.frames on athlete's name and output/return successful result
+  team.statistics = merge(team.statistics_batch_01, team.statistics_batch_02,
+                          by='athlete_display_name')
+  print(glue('  == SUCCESS == {team.name} {valid.start - 1} data gathered'))
   
-  home.prevszn_stats = merge(home.big_data, home.shooting_data, by='athlete_display_name')
-  print(glue('  > SUCCESS: {home.team} {valid.start - 1} data gathered'))
-  
-  
-  
-  home.current_season = load_wbb_schedule(valid.start + 1) %>% filter(home.id == home.info$team_id)
-  
-  teams.current_played = home.current_season %>%
-    mutate(date = as.Date(date)) %>%
-    filter(date < Sys.Date()) %>%
-    select(id)
-  
-  print(glue('  > Extracting {valid.start} data for {home.team} ...'))
-  home.played_list = apply(teams.current_played, 1, espn_wbb_game_all)
-  
-  print(glue('  > Merging {home.team} {valid.start} data.frames ...'))
-  current_df_list = list()
-  for (i in 1:length(teams.current_played)) {
-    current_df_list[[i]] = home.played_list[[i]]$Player
-  }
-  
-  home.current_data = dplyr::bind_rows(current_df_list) %>%
-    filter(team_id == home.info$team_id) %>%
-    select(athlete_display_name, min, pts,
-           reb, ast) %>%
-    group_by(athlete_display_name) %>%
-    summarise(
-      GP = n(),
-      MIN = round(mean(as.numeric(min), na.rm=TRUE), 1),
-      PTS = round(mean(as.numeric(pts), na.rm=TRUE), 1),
-      RBS = round(mean(as.numeric(reb), na.rm=TRUE), 1),
-      AST = round(mean(as.numeric(ast), na.rm=TRUE), 1)
-    )
-  
-  home.current_shooting_data = dplyr::bind_rows(current_df_list) %>%
-    filter(team_id == home.info$team_id) %>%
-    select(athlete_display_name, fg, ft) %>%
-    extract(fg, c('fga', 'fgm'), '(.)-(.)') %>%
-    extract(ft, c('fta', 'ftm'), '(.)-(.)') %>%
-    group_by(athlete_display_name) %>%
-    summarise(
-      FGA = sum(as.numeric(fga)),
-      FGM = sum(as.numeric(fgm)),
-      FTA = sum(as.numeric(fta)),
-      FTM = sum(as.numeric(ftm))
-    ) %>%
-    mutate(fgp = round(FGA / (FGA + FGM) * 100, 1),
-           ftp = round(FTA / (FTA + FTM) * 100, 1)) %>%
-    select(athlete_display_name, fgp, ftp)
-  
-  home.current_stats = merge(home.current_data, home.current_shooting_data, by='athlete_display_name')
-  print(glue('  > SUCCESS: {home.team} {valid.start - 1} data gathered'))
-  
-  write.xlsx(home.current_stats, glue('{home.team}-{away.team}-{Sys.Date()}.xlsx'))
+  return(team.statistics)
 }
 
+format_team_data = function(previous_dataframe, lastfive_dataframe, current_dataframe) {
+  previous_dataframe %>%
+    dplyr::bind_rows(lastfive_dataframe) %>%
+    dplyr::bind_rows(current_dataframe) %>%
+    arrange(athlete_display_name) %>%
+    select(athlete_display_name, season, GP, MIN,
+           PTS, RBS, AST, fgp, ftp)
+}
+
+main = function() {
+  start.input = user.input('Enter season end year (e.g. 2022 for 2021-22 season): ')
+  valid.start = validate_date(as.numeric(start.input))
+  home.input = str_to_title(user.input('Enter home team: '))
+  away.input = str_to_title(user.input('Enter away team: '))
+  valid.query = validate_query(home.input, away.input)
+  
+  # assign home team names
+  home.team = valid.query[[1]][1]
+  away.team = valid.query[[2]][1]
+  
+  # extract previous season and current season data.frames per team
+  home.current_dataframe = extract_team_data(home.team, valid.start, 'current') %>%
+    mutate(season = glue('{valid.start-1}-{valid.start-0-2000}'))
+  home.lastfive_dataframe = extract_team_data(home.team, valid.start, 'lastfive') %>%
+    mutate(season = 'Previous5')
+  home.previous_dataframe = extract_team_data(home.team, valid.start, 'previous') %>%
+    filter(athlete_display_name %in% home.current_dataframe$athlete_display_name) %>%
+    mutate(season = glue('{valid.start-2}-{valid.start-1-2000}'))
+  away.current_dataframe = extract_team_data(away.team, valid.start, 'current') %>%
+    mutate(season = glue('{valid.start-1}-{valid.start-0-2000}'))
+  away.lastfive_dataframe = extract_team_data(away.team, valid.start, 'lastfive') %>%
+    mutate(season = 'Previous5')
+  away.previous_dataframe = extract_team_data(away.team, valid.start, 'previous') %>%
+    filter(athlete_display_name %in% home.current_dataframe$athlete_display_name) %>%
+    mutate(season = glue('{valid.start-2}-{valid.start-1-2000}'))
+ 
+  home.results = format_team_data(home.previous_dataframe,
+                                  home.lastfive_dataframe,
+                                  home.current_dataframe)
+  away.results = format_team_data(away.previous_dataframe,
+                                  away.lastfive_dataframe,
+                                  away.current_dataframe)
+  
+  wb = createWorkbook()
+  home.data = createSheet(wb, sheetName='Home_Data')
+  addDataFrame(home.results, home.data)
+  away.data = createSheet(wb, sheetName='Away_Data')
+  addDataFrame(away.results, away.data)
+  saveWorkbook(wb, glue('{home.team}-{away.team}-{Sys.Date()}.xlsx'))
+  
+}
+
+valid.teams = validate_teams() %>% select(-X)
 main()
